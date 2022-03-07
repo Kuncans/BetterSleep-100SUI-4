038c6e5ae6f0dda09dbf32458e2bc548f6309f33
Kuncans <akafauna@gmail.com>
2022-03-07 20:10:54 +0000

    Initial Commit

---
+++ BetterSleep-100SUI-4/BetterSleep_100SUI_4App.swift
@@ -0,0 +1,17 @@
+//
+//  BetterSleep_100SUI_4App.swift
+//  BetterSleep-100SUI-4
+//
+//  Created by Duncan Kent on 07/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterSleep_100SUI_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
